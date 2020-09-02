bc89dee612c729d0c603dd87c5340b73b2eb4d52
Richard Kurwitz <kurwitz3@aol.com>
2020-09-02 17:10:08 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,7 @@
 class User 
-  attr_accessor :first, :last 
+  attr_accessor :first_name, :last_name 
    def initialize
-     @first = first 
-     @last = last 
+     @first_name = first_name 
+     @last_name = last_name 
    end 
  end 
